d88e85019306fc7a82ea6dea95656fa5c373ad7a
Cristian Valencia <cristian.valencia@MacBook-Pro-di-Cristian.local>
2021-01-13 23:09:11 +0100

    added Carousel and did some CSS

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.onload=function()
-{
+{   
 
     const burgerMenuBtn = document.querySelector(".burgerMenuContainer");
 
@@ -15,7 +15,52 @@ window.onload=function()
 
         document.querySelector(".three").classList.toggle("threeX");
 
+        document.querySelector(".headerText").classList.toggle("headerTextDown")
+
     });
+
+    const slider = () => {
+    
+        //console.log(document.querySelector(".spostati"))
+        
+        if(document.querySelector(".fadeOut")!=null)
+        {
+            document
+                .querySelector(".fadeOut")
+                .classList
+                .remove("fadeOut");            
+        }
+    
+        
+        const imgVisibile=document
+            .querySelector(".carouselContainer .visible")
+    
+      //  console.log(imgVisibile)
+      //  console.log(imgVisibile.nextElementSibling)
+        
+        
+        imgVisibile.classList.remove("visible");
+        imgVisibile.classList.add("fadeOut");
+        
+        if(imgVisibile.nextElementSibling!=null)
+        {
+            imgVisibile
+                .nextElementSibling
+                .classList.add("visible");        
+        }
+        else{
+            //var genitore=imgVisibile.parentElement;
+            //genitore.children[0].classList.add("visibile");
+            
+            document
+                .querySelector(".carouselContainer img:nth-of-type(1)")
+                .classList.add("visible")
+            
+        }
+        
+    }
+
+    setInterval(slider,3000);
 
     
 
